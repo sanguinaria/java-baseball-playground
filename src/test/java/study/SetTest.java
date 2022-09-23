4bb9cfc49608f154bdddc9286f084772f422d798
sanguinaria <jwshim80@gmail.com>
2022-09-23 13:43:41 +0900

    feat: SetTest Requirement 2
    Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
    구현하고 보니 다음과 같이 중복 코드가 계속해서 발생한다.
    JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 
@@ -25,6 +27,12 @@ public class SetTest {
   @Test
   void check_size() {
     assertThat(numbers.size()).isEqualTo(3);
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {1, 2, 3})
+  void contains(int number) {
+    assertThat(numbers.contains(number)).isTrue();
   }
 
 }
