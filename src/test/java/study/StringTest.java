945b34d2df1cd383ed135143dfaaacd277a87af5
sanguinaria <jwshim80@gmail.com>
2022-09-23 13:27:30 +0900

    feat: StringTest Requirement 3
    "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
    String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
    JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,12 @@
 package study;
 
+import java.util.Random;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -27,6 +31,26 @@ public class StringTest {
     void substring_parentheses()    {
         String actual = "(1,2)".substring(1, 4);
         assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자를 가져온다")
+    void charAt()   {
+        String value = "abc";
+        int position = new Random().nextInt(value.length() - 1);
+        char actual = value.charAt(position);
+        assertThat(actual).isEqualTo(value.toCharArray()[position]);
+    }
+
+    @Test
+    @DisplayName("위치 값을 벗어나면 예외 발생")
+    void charAt_out_of_range()   {
+        String value = "abc";
+        int position = new Random().nextInt() + value.length();
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+            .isThrownBy(() -> {
+                char actual = value.charAt(position);
+            });
     }
 
 
